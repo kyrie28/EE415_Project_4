50728bcfb1052bd6e99ed501926128c6612beb22
최병현 <harryasd@kaist.ac.kr>
2020-04-14 16:23:31 -0700

    submit

---
+++ pintos/src/threads/fixed_point.h
@@ -0,0 +1,102 @@
+#define F (1 << 14) // fixed point 1
+#define INT_MAX ((1 << 31) -1)
+#define INT_MIN (-(1 << 31))
+
+// x and y denote fixed_point numbers in 17.14 format
+// n is an integer
+
+int int_to_fp (int n);
+int fp_to_int_round (int x);
+int fp_to_int (int x);
+
+int add_fp (int x, int y);
+int add_mixed (int x, int n);
+
+int sub_fp (int x, int y);
+int sub_mixed (int x, int n);
+
+int mult_fp (int x, int y);
+int mult_mixed (int x, int n);
+
+int div_fp (int x, int y);
+int div_mixed (int x, int n);
+
+// int to fp
+int
+int_to_fp (int n)
+{
+  return n * F;
+}
+
+// fp to int (rounding to nearest)
+int
+fp_to_int_round (int x)
+{
+  if (x >= 0)
+    return (x + F / 2) / F;
+  else
+    return (x - F / 2) / F;
+}
+
+// fp to int (rounding toward zero)
+int
+fp_to_int (int x)
+{
+  return x / F;
+}
+
+// + between fp numbers
+int
+add_fp (int x, int y)
+{
+  return x + y;
+}
+
+// + between fp number and int
+int
+add_mixed (int x, int n)
+{
+  return x + n * F;
+}
+
+// - between fp numbers
+int
+sub_fp (int x, int y)
+{
+  return x - y;
+}
+
+// - between fp number and int
+int
+sub_mixed (int x, int n)
+{
+  return x - n * F;
+}
+
+// * between fp numbers
+int
+mult_fp (int x, int y)
+{
+  return ((int64_t) x) * y / F;
+}
+
+// * between fp number and int
+int
+mult_mixed (int x, int n)
+{
+  return x * n;
+}
+
+// / between fp numbers
+int
+div_fp (int x, int y)
+{
+  return ((int64_t) x) * F / y;
+}
+
+// / between fp number and int
+int
+div_mixed (int x, int n)
+{
+  return x / n;
+}
